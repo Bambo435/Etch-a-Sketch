e0b025b70cfe915950ecb0de4efb19d7126dfee2
Sean Malone <seanmalone1993@gmail.com>
2022-03-29 21:54:12 +0100

    Add function for resetting etch-a-sketch

--- EAS.js
+++ EAS.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#container');
+let container = document.querySelector('#container');
 
 //  we create a 'grid' of divs to be later turned to squares via CSS.
 function makeGrid(num) {
@@ -7,17 +7,28 @@ function makeGrid(num) {
     div.id = 'row';
     container.appendChild(div);
     for (j = 0; j < num; j++) {
-      let div2 = document.createElement('div'); // As well as appending a set of 16 divs onto a row, we also attach an event listener to convert a square to red when the mouse hovers over it.
+      let div2 = document.createElement('div'); // As well as appending a set of 16 divs onto a row, we also attach an event listener to each square so that every square will change colour.
       div2.classList.add('square');
       div2.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = 'red';
+        e.target.style.backgroundColor = 'purple';
       });
-      div2.id = 'column';
       div.appendChild(div2);
 
   }
 }
 };
-makeGrid(50);
 
+makeGrid(50); // default number of squares that will appear upon loading page
+
+function reset() {
+  let input = prompt('How many squares? (50 = 50x50)', '');
+  const divs = document.getElementById('container');
+  divs.textContent = '';
+  makeGrid(input);
+};
+
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', reset); 
 
